0c6a0802f4895e2d397ba87f87eedd2f9f406f76
Da Teng <dateng2016@gmail.com>
2025-01-25 18:14:49 -0500

    a

--- Scratch.java
+++ Scratch.java
@@ -1,7 +1,27 @@
+import pkg.Human;
+
 public class Scratch {
     public static void main(String[] args) throws ClassNotFoundException {
-        String s = "Hello world!";
-        
-        System.out.println(Class.forName("pkg.Human"));;
+        String classFullName = "pkg.Human";
+
+        // System.out.println(Class.forName("pkg.Human"));;
+
+        // Method 1
+        Class clazz1 = Class.forName(classFullName);
+
+        System.out.println(clazz1);
+
+        // Method 2
+        Class clazz2 = Human.class;
+
+        System.out.println(clazz1 == clazz2);
+
+        // Method 3
+        Human h = new Human();
+        Class clazz3 = h.getClass();
+
+        System.out.println(clazz1 == clazz3);
+
+
     }
 }
