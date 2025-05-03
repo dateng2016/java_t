d02bcabb976e9f6890c478d7707828581f483141
Da Teng <dateng2016@gmail.com>
2025-05-03 19:24:43 -0400

    a

--- Scratch.java
+++ Scratch.java
@@ -1,20 +1,4 @@
-import java.util.ArrayList;
-
 public class Scratch {
-
     public static void main(String[] args) {
-        ArrayList<String> ls = new ArrayList<>();
-        ls.add("John");
-        ls.add("Wick");
-
-        ArrayList<String> ls2 = new ArrayList<>();
-        for (var name : ls) {
-            if (name.startsWith("Joh")) {
-                ls2.add(name);
-            }
-        }
-
-        System.out.println(ls2);
-
     }
 }
